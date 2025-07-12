b34dc2670f4791e174c72b9eda14c810ef76170a
rythmn1111 <82413584+rythmn1111@users.noreply.github.com>
2025-07-12 07:31:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/dashboard/subscriptions/components/payment-method-details.tsx
@@ -0,0 +1,34 @@
+import { PaymentMethodDetails as PaddlePaymentMethodDetails } from '@paddle/paddle-node-sdk';
+import { CreditCard } from 'lucide-react';
+
+const PaymentMethodLabels: Record<PaddlePaymentMethodDetails['type'], string> = {
+  card: 'Card',
+  alipay: 'Alipay',
+  wire_transfer: 'Wire Transfer',
+  apple_pay: 'Apple Pay',
+  google_pay: 'Google Pay',
+  paypal: 'PayPal',
+  ideal: 'iDEAL',
+  bancontact: 'Bancontact',
+  korea_local: 'Korean Local Payment',
+  offline: 'Offline',
+  unknown: 'Unknown',
+};
+
+interface Props {
+  type: PaddlePaymentMethodDetails['type'];
+  card?: PaddlePaymentMethodDetails['card'];
+}
+
+export function PaymentMethodDetails({ type, card }: Props) {
+  if (type === 'card') {
+    return (
+      <>
+        <CreditCard size={18} />
+        <span className={'text-base text-secondary leading-4'}>**** {card?.last4}</span>
+      </>
+    );
+  } else {
+    return type ? <span className={'text-base text-secondary leading-4'}>{PaymentMethodLabels[type]}</span> : '-';
+  }
+}
